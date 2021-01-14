8b39ee18058baab6afcfd1a2295c81efeb3d2143
Mohammed Powder <mpowder@un-d2.kzn.21-school.ru>
2021-01-14 23:40:13 +0300

    last from school

--- map_validation.c
+++ map_validation.c
@@ -6,7 +6,7 @@
 /*   By: mpowder <mpowder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 23:45:08 by mpowder           #+#    #+#             */
-/*   Updated: 2020/12/14 01:59:06 by mpowder          ###   ########.fr       */
+/*   Updated: 2020/12/23 07:54:01 by mpowder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,24 @@ static int	ft_map_size(char **map)
 	return (size);
 }
 
-void		ft_map_validation(char **map)
+static void	ft_border(char **map)
 {
 	int	i;
-	int	j;
+	// int	j;
+
+	i = -1;
+	while ((*map)[++i])
+		if ((*map)[i] != ' ' && (*map)[i] != '1')
+			ft_exit(1, MAP_ERROR);
+	
+}
+
+void		ft_map_validation(char **map)
+{
+	// int	i;
+	// int	j;
 	int	size;
 
 	size = ft_map_size(map);
-	printf("%d\n", size);
-	while (i < size)
-		printf("%s\n", map[i++]);
+	ft_border(map);
 }
